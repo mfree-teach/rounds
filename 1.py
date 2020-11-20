4ef9a7d18de5ab3059fdb707b1d729c1877d518e
miss.optimizm <miss.optimizm@gmail.com>
2020-11-20 17:35:54 +0500

    сделали случайные цвета

--- 1.py
+++ 1.py
@@ -1,16 +1,30 @@
 import sys
 from PyQt5 import uic
-from PyQt5.QtCore import Qt
-from PyQt5.QtGui import QPainter
+from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QWidget, QApplication
-from random import random
+from random import random, randrange
 
 
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.pushButton = QtWidgets.QPushButton(Form)
+        self.pushButton.setGeometry(QtCore.QRect(130, 260, 171, 23))
+        self.pushButton.setObjectName("pushButton")
 
-class Window(QWidget):
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.pushButton.setText(_translate("Form", "Нажми"))
+
+class Window(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
         self.fl = False
 
@@ -23,10 +37,11 @@ class Window(QWidget):
             d, x, y = round(random() * self.width()), round(random() * self.width() / 2), round(
                 random() * self.width() / 2)
             l = [x, y, d, d]
+            color = QColor(randrange(256), randrange(256), randrange(256))
             qp = QPainter()
             qp.begin(self)
 
-            qp.setBrush(Qt.yellow)
+            qp.setBrush(color)
             qp.drawEllipse(*l)
 
             qp.end()
